705cacc7834da59b9af45ed2fd43bb829b911736
ValentinaCastaño <lvcastanomnal.edu.co>
2021-05-02 13:29:22 -0500

    strore configure

--- redux/store.ts
+++ redux/store.ts
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { HYDRATE, createWrapper } from 'next-redux-wrapper'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk'
 import { userReducer} from './reducers/user'
 import { packageReducer} from './reducers/package'
@@ -13,4 +14,4 @@ const reducers = combineReducers({
 })
 
 //export const store = createStore(userReducer)
-export const store = createStore(reducers)
+export const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunkMiddleware)))
